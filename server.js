9a7493e90a08d257d07d57c7a3ae57cdd74047e1
Thorsten Lorenz <thlorenz@gmx.de>
2012-11-10 18:03:26 -0500

    socket io skeleton

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
-var http = require('http');
-var ecstatic = require('ecstatic')(__dirname+'/bookmarklet');
+var port = 3000;
 
-http.createServer(ecstatic).listen(3000);
+var http     =  require('http');
+var ecstatic =  require('ecstatic')(__dirname+'/bookmarklet');
 
-console.log(__dirname);
-console.log('Listening on: http://localhost:3000');
+var server = http.createServer(ecstatic).listen(port);
+var io = require('socket.io').listen(server);
 
+console.log('Listening on: http://localhost:', port);
+
+io.sockets.on('connection', function (socket) {
+  socket.on('sound', function (data) {
+    console.log(data);
+  });
+});
 
 ///setup socket io to proxy sound buffers through to all clients
