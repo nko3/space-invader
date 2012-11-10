e074f6cdd1949c75384639966532729622965a2e
Janessa Det <jandet@gmail.com>
2012-11-10 12:47:01 -0500

    make real audio src node from file

--- spikes/audio-capture/js/index.js
+++ spikes/audio-capture/js/index.js
@@ -9,10 +9,29 @@ navigator.webkitGetUserMedia({audio: true}, function(stream) {
   var microphone = context.createMediaStreamSource(stream);
   var scriptProcessor = context.createScriptProcessor(2048);
   scriptProcessor.onaudioprocess = onaudioprocess;
+  console.log(scriptProcessor);
 
-  microphone.connect(scriptProcessor);
-  scriptProcessor.connect(context.destination);
-  context.startRendering();
+  var hello = 'http://thelab.thingsinjars.com/web-audio-tutorial/hello.mp3';
+  var request = new XMLHttpRequest();
+  request.open("GET", hello, true);
+  request.responseType = "arraybuffer";
+
+  request.onload = function() {
+    var audio = request.response;
+    var sound = context.createBufferSource();
+    sound.buffer = context.createBuffer(audio, true);
+    sound.loop = true;
+    
+    sound.connect(scriptProcessor);
+    sound.connect(context.destination);
+    sound.start(0);
+  };
+
+  request.send();
+
+  // microphone.connect(scriptProcessor);
+  // scriptProcessor.connect(context.destination);
+  // context.startRendering();
   function onaudioprocess(event){
     for (var i = 0; i < 2; i++) {
       var data = event.outputBuffer.getChannelData(i);
