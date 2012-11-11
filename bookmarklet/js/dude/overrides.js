78ab97e06a6c9cff3bbb113548553e6fc1e33c3a
Domenic Denicola <domenic@domenicdenicola.com>
2012-11-11 12:02:45 -0500

    Add dude.vel.

--- bookmarklet/js/dude/overrides.js
+++ bookmarklet/js/dude/overrides.js
@@ -5,6 +5,11 @@ function setGotoSpeed (speed_) {
   me.overrides.speed = speed_;
 }
 
+nko.Vector.prototype.normalize = function () {
+  var length = this.length();
+  return new nko.Vector(this.x / length, this.y / length);
+};
+
 function init (nko) {
   me = nko.me;
   me.overrides = { speed: 200 };
@@ -18,6 +23,8 @@ function init (nko) {
     var self = this
       , delta = pos.minus(this.pos)
       , duration = duration !== undefined && typeof(duration) !== 'function' ? duration : delta.length() / this.overrides.speed * 1000;
+
+    this.vel = delta.normalize().times(speed);
 
     this.animate(delta.cardinalDirection());
     if (duration && duration > 0)
@@ -42,6 +49,7 @@ function init (nko) {
         },
         complete: function() {
           self.pos = pos;
+          self.vel = new nko.Vector(0, 0);
           if (callback) callback();
         }
       });
