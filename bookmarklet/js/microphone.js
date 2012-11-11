94b18ac9db2a8bb7e3069301394bd26e2d05839d
Domenic Denicola <domenic@domenicdenicola.com>
2012-11-10 19:06:27 -0500

    Only send audio while the Ctrl key is held down.

--- bookmarklet/js/microphone.js
+++ bookmarklet/js/microphone.js
@@ -1,6 +1,16 @@
 var microphoneData = require('./microphone-data');
 var socket = io.connect('http://' + window.location.hostname + ':3000');
 
+var ctrlKeyIsDown = false;
+$(window).on({
+  'keydown': function (e) {
+    ctrlKeyIsDown = e.ctrlKey;
+  },
+  'keyup': function (e) {
+    ctrlKeyIsDown = e.ctrlKey;
+  }
+});
+
 socket.on('welcome', function (data) {
   console.log('welcome', data);
 
@@ -9,19 +19,20 @@ socket.on('welcome', function (data) {
   }
 
   navigator.webkitGetUserMedia(
-    { audio: true }, 
-    function(stream) { microphoneData(stream, processData); }, 
+    { audio: true },
+    function(stream) { microphoneData(stream, processData); },
     onError
   );
 
   function processData(data) {
-    var id = nko.me.id;
-    socket.emit('sound', { buffer: data, id: id });
+    if (ctrlKeyIsDown) {
+      var id = nko.me.id;
+      socket.emit('sound', { buffer: data, id: id });
+      console.log('sending sound to the server');
+    }
   }
 });
 
 socket.on('sound', function (data) {
   console.log('data', data);
 });
-
-
