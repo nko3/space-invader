f8179196aa1b486e6f7844f29911a1c8b0881346
John Paul <john@johnkpaul.com>
2012-11-11 16:17:27 -0500

    calculate correct path in config dependeing on window.location

--- bookmarklet/js/config.js
+++ bookmarklet/js/config.js
@@ -1,3 +1,12 @@
+var path;
+
+if (window.location.href.indexOf('nodeknockout') > 0){
+  path = 'http://space-invader.nko3.jit.su/';
+}
+else{
+  path = 'http://localhost:3000/';
+}
+
 module.exports = {
-  path:'http://localhost:3000/'
+  path:path
 };
