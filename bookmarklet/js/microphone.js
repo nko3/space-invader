fa3ccc343f3713698f9a0745154b48cbcf70e57f
Domenic Denicola <domenic@domenicdenicola.com>
2012-11-10 19:13:11 -0500

    Refactor.

--- bookmarklet/js/microphone.js
+++ bookmarklet/js/microphone.js
@@ -2,28 +2,29 @@ var microphoneData = require('./microphone-data');
 var ctrlKeyIsDown = require('./ctrl-key-is-down');
 var socket = io.connect('http://' + window.location.hostname + ':3000');
 
+function onError(err) {
+  throw err;
+}
+
+// Sending data up
 socket.on('welcome', function (data) {
   console.log('welcome', data);
 
-  function onError(err) {
-    console.dir(err);
-  }
+  navigator.webkitGetUserMedia({ audio: true }, onData, onError);
 
-  navigator.webkitGetUserMedia(
-    { audio: true },
-    function(stream) { microphoneData(stream, processData); },
-    onError
-  );
+  function onData(stream) {
+    microphoneData(stream, processData);
+  }
 
   function processData(data) {
     if (ctrlKeyIsDown()) {
-      var id = nko.me.id;
-      socket.emit('sound', { buffer: data, id: id });
       console.log('sending sound to the server');
+      socket.emit('sound', { buffer: data, id: nko.me.id });
     }
   }
 });
 
+// Receiving data
 socket.on('sound', function (data) {
   console.log('data', data);
 });
