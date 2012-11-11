1cd17dd1d365eae90a43513ae462e3d766553fcc
Domenic Denicola <domenic@domenicdenicola.com>
2012-11-11 18:21:18 -0500

    XDomain

--- server.js
+++ server.js
@@ -1,18 +1,30 @@
+'use strict';
+
 var port = 3000;
 
-var http = require('http');
 var express = require('express');
 var routes = require('./routes');
 var app = express();
-app.configure(function(){
+
+function allowCrossDomain(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+  next();
+}
+
+
+app.configure(function () {
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
+  app.use(allowCrossDomain);
   app.use(app.router);
   app.use(express.static('bookmarklet'));
 });
-var server = app.listen(port)
+var server = app.listen(port);
 
 app.get('/', routes.index);
 app.get('/mp3', routes.mp3);
